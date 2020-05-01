4e148cfeb86306666e0301f05bea30e40ff526a8
Wares <fazelyar.wares@gmail.com>
2020-05-01 04:58:14 +0000

    parameterized queries for student.js and teacher.js to protect against potentially mailicious sql injections

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,38 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-// THE OBJECT THAT RETRNS FROM THE QUERY FUNCTION
-// pool.query(`
-// SELECT id, name, cohort_id
-// FROM students
-// LIMIT 5;
-// `)
-// .then(res => {
-//   console.log(res.rows);
-// })
-// .catch(err => console.error('query error', err.stack));
-
-// RETURNING THE QUERY'S RESULT OBJECTS AS JAVASCRIPT STRING
-// pool.query(`
-// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-// FROM students
-// JOIN cohorts ON cohorts.id = cohort_id
-// LIMIT 5;
-// `)
-// .then(res => {
-//   res.rows.forEach(user => {
-//     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-//   })
-// });
-
-// ALTERED CODE TO SO QUERY TAKES CUSTOM PARAMETERS FROM COMMAND LINE
-pool.query(`
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// store potentially malicious values
+const values = [`%${cohortName}%`, limit]
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2
+`;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
